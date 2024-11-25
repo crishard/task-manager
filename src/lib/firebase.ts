4251d5bce082634741d93c3d9ec3d0e1fea03012
crishard <crislantorres9@gmail.com>
2024-11-25 09:37:41 -0300

    configurando firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { browserSessionPersistence, getAuth, GoogleAuthProvider, setPersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+setPersistence(auth, browserSessionPersistence)
+  .catch((error) => console.error('Persistência de sessão falhou', error));
